b388bb49a3234447efd71c598ef16b5fd4a59274
ppillot <paulpillot@gmail.com>
2017-04-10 09:33:26 -0400

    help styles fix

--- src/components/Help.vue
+++ src/components/Help.vue
@@ -1,5 +1,8 @@
 <template>
-  <div class="help" v-html="text">
+  <div>
+    <hr class="shadow">
+    <div class="help" v-html="text">
+    </div>
   </div>
 </template>
 
@@ -27,5 +30,32 @@
 </script>
 
 <style>
-  
+  .shadow {
+    border-top: solid 1px #eee;
+    border-bottom: none;
+    /* box-shadow: #bbb 0px 4px 10px; */
+    margin: 8px 0 0 0;
+  }
+  .help {
+    flex: 1;
+    overflow-y: auto;
+    font-size: 0.9em;
+  }
+  .help h1 {
+    font-size: 1.1em;
+    margin: 0.3em 0;
+  }
+  .help p {
+    margin-top: 0.3em;
+  }
+  .help a {
+    background: #EFF2F7;
+    color: #20A0FF;
+    text-decoration-style: dotted;
+    text-decoration-line: underline;
+  }
+  .help a:hover {
+    text-decoration-line: none;
+    background: none;
+  }
 </style>

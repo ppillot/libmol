e45fa525c74e47e8ee5fd5628c4492357f4650d0
ppillot <paulpillot@gmail.com>
2017-05-26 21:17:04 -0400

    wip replacing el-autocomplete

--- src/components/Searchlibmol.vue
+++ src/components/Searchlibmol.vue
@@ -1,6 +1,28 @@
 <template>
-  <form-item :label="$t('ui.search_libmol_label')">
-    <el-autocomplete v-model="state" :fetch-suggestions="debouncedQuery" placeholder="Mot clé" @select="handleSelect"></el-autocomplete>
+  <form-item :label="$t('ui.search_libmol_label')"
+    @blur="activate(false)">
+    <div class="input-text" >
+      <input type="text"
+        spellcheck="false"
+        :value="state"
+        placeholder="Mot clé"
+        @keyup="getSuggestion"
+        @focus="activate(true)"
+      >
+      <div class="suggest" :style="suggestStyles" v-if="isFocused">
+        <ul>
+          <li v-for="(suggestion, index) in suggestions" @click="handleSelect(index)">
+            {{ suggestion.value }}
+          </li>
+        </ul>
+      </div>
+    </div>
+    <!--<el-autocomplete
+      v-model="state"
+      :fetch-suggestions="debouncedQuery"
+      placeholder="Mot clé"
+      @select="handleSelect">
+    </el-autocomplete>-->
   </form-item>
 </template>
 
@@ -16,12 +38,31 @@ export default {
   },
   data () {
     return {
-      links: [],
-      state: '',
-      timeout: null
+      suggestions: [],
+      timeout: null,
+      isFocused: false,
+      state: ''
+    }
+  },
+  computed: {
+    suggestStyles: function () {
+      let rect = this.$el.getBoundingClientRect()
+      return {
+        top: rect.bottom + 'px',
+        left: rect.left + 'px',
+        'min-width': rect.width + 'px',
+        visibility: (this.suggestions.length === 0) ? 'hidden' : 'visible'
+      }
     }
   },
   methods: {
+    activate: function (val = true) {
+      this.isFocused = val
+    },
+    getSuggestion: function (event) {
+      this.state = event.target.value
+      this.debouncedQuery(this.state)
+    },
     debouncedQuery: debounce(
       300,
       function (q, c) {
@@ -30,7 +71,7 @@ export default {
 
     querySearchAsync (queryString, cb) {
       if (queryString.length === 0) {
-        this.links.splice(0)
+        this.suggestions.splice(0)
         return
       }
       const path = (process.env.NODE_ENV !== 'production') ? 'api/recherche.php' : 'https://libmol.org/api/recherche.php'
@@ -58,24 +99,84 @@ export default {
           txt: queryString
         })
       })
-      .then(function (response) {
+      .then(response => {
         return response.json()
       })
-      .then(function (data) {
-        const rep = data.map(item => ({ value: item.label,
+      .then(data => {
+        this.suggestions = data.map(item => ({ value: item.label,
           file: ((item.file.indexOf('.cif') > -1) || (item.file.indexOf('.mmtf') > -1) || (item.file.indexOf('.sdf') > -1))
             ? 'static/mol/' + item.file
             : `static/mol/pdb/${item.file}.pdb`,
           molId: item.molId }))
-        cb(rep)
+        // cb(rep)
       })
       .catch(function (error) {
         console.log(error)
       })
     },
-    handleSelect (item) {
-      this.$store.dispatch('loadNewFile', item)
+    handleSelect (index) {
+      this.$store.dispatch('loadNewFile', this.suggestions[index])
     }
   }
 }
 </script>
+
+<style>
+  .input-text {
+    width: 100%;
+    display: inline-block;
+    border-radius: 3px;
+    border: 1px solid #D3DCE6;
+    font-size: 1em;
+    box-sizing: border-box;
+    padding: 0.5em;
+  }
+  .input-text input::placeholder {
+    color: #99A9BF;
+  }
+  .input-text input {
+    box-sizing: border-box;
+    font-size: 1em;
+    outline: none;
+    width: 100%;
+    border: none;
+  }
+  .suggest {
+    position: fixed;
+    width: 30em;
+    background: white;
+    /* border: 1px solid #d1dbe5; */
+    z-index: 3;
+    border-radius: 4px;
+    margin: -1px 0 0 6px;
+    max-height: 15em;
+    overflow: scroll;
+    box-shadow: 0 1px 3px #aaa;
+  }
+
+  .suggest .category {
+    background: #324057;
+    font-weight: 600;
+    color: #fff;
+    padding: 0.2em 0.5em 0.2em 0.5em;
+  }
+
+  .suggest ul {
+    width: 100%;
+    margin: 0 0 0.5em 0;
+    list-style: none;
+    padding: 0;
+    overflow: hidden;
+  }
+  .suggest ul li {
+    width: 100%;
+    padding: 0.5em;
+    cursor: pointer;
+    color: #8492a6;
+  }
+  .suggest ul li:hover {
+    background: #20A0FF;
+    color: #FFF;
+  }
+</style>
+

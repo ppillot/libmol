1e6eb99010935f142ae53f8db4369bb2e871844d
ppillot <paulpillot@gmail.com>
2019-04-06 22:55:33 -0400

    libraries config

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,22 @@ module.exports = {
     '@vue/typescript'
   ],
   rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-console': 'off',
+    'no-debugger': 'off',
+    'vue/attribute-hyphenation': 'warning',
+    'vue/html-closing-bracket-newline': 'warning',
+    'vue/html-quotes': 'error',
+    'vue/multiline-html-element-content-newline': 'warning',
+    'vue/mustache-interpolation-spacing': 'error',
+    'vue/name-property-casing': 'error',
+    'vue/no-spaces-around-equal-signs-in-attribute': 'error',
+    'vue/no-template-shadow': 'error',
+    'vue/prop-name-casing': 'error',
+    'vue/require-default-prop': 'warning',
+    'vue/require-prop-types': 'warning',
+    'vue/no-v-html': 'warning',
+    'vue/this-in-template': 'warning',
+    'vue/camelcase': 'error'
   },
   parserOptions: {
     parser: '@typescript-eslint/parser'
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -1,8 +1,16 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button, Dialog, Icon, Popover, Scrollbar, Slider, Switch, Tabs, TabPane } from 'element-ui'
 import lang from 'element-ui/lib/locale/lang/en'
 import locale from 'element-ui/lib/locale'
 
 locale.use(lang)
 
 Vue.use(Button)
+Vue.use(Dialog)
+Vue.use(Icon)
+Vue.use(Popover)
+Vue.use(Scrollbar)
+Vue.use(Slider)
+Vue.use(Switch)
+Vue.use(Tabs)
+Vue.use(TabPane)

a63cc05c159910d277ec5a14fd4fceb68cd299e9
ppillot <paulpillot@gmail.com>
2019-04-06 22:56:30 -0400

    fix libraries calls

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from './store'
 import './plugins/element.js'
+import VueI18n from 'vue-i18n'
 
 Vue.config.productionTip = false
+Vue.use(VueI18n)
 
 new Vue({
   store,

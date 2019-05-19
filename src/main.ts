20930a221b3158430406e9ebf56df78c724f4636
ppillot <paulpillot@gmail.com>
2019-05-19 11:02:31 -0400

    fix: locales not properly set

--- src/main.ts
+++ src/main.ts
@@ -3,13 +3,11 @@ import App from './App.vue'
 import store from './store/index'
 import './plugins/element'
 import VueI18n from 'vue-i18n'
-import { locales } from './locales/locales'
+import { locales as localesList } from './locales/locales'
 import './assets/font/css/icons.css'
 
 Vue.config.productionTip = false
 Vue.use(VueI18n)
-
-let app: Vue
 
 let langList = [navigator.language]
 if (navigator.languages) langList.push(...navigator.languages)
@@ -18,9 +16,14 @@ const langSet = new Set(
     return l.substr(0, 2).toLowerCase()
   })
 )
-const langFound = locales.find(locale => {
-  return langSet.has(locale)
-})
+let langFound: string
+for (const locale of langSet) {
+  if (localesList.includes(locale)) {
+    langFound = locale
+    break
+  }
+}
+
 const lang = (langFound === undefined) ? 'en' : langFound
 
 import('./locales/bundles/' + lang + '.json')

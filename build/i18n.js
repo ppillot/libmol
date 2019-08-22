fff6b75f962ae1d5726e5ba8cbe4a8d10219eac2
ppillot <paulpillot@gmail.com>
2019-08-21 23:28:56 -0400

    fix call to google API for i18n

--- build/i18n.js
+++ build/i18n.js
@@ -1,26 +1,26 @@
-var fs = require('fs');
-var readline = require('readline');
-var google = require('googleapis');
-var googleAuth = require('google-auth-library');
+var fs = require('fs')
+var readline = require('readline')
+const { google } = require('googleapis')
+// var googleAuth = require('google-auth-library')
 var escapeQuotes = require('escape-quotes')
 
 // If modifying these scopes, delete your previously saved credentials
 // at ~/.credentials/sheets.googleapis.com-nodejs-quickstart.json
-var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
+var SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 var TOKEN_DIR = (process.env.HOME || process.env.HOMEPATH ||
-    process.env.USERPROFILE) + '/.credentials/';
-var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json';
+    process.env.USERPROFILE) + '/.credentials/'
+var TOKEN_PATH = TOKEN_DIR + 'sheets.googleapis.com-nodejs-quickstart.json'
 
 // Load client secrets from a local file.
-fs.readFile('./build/client_secret.json', function processClientSecrets(err, content) {
+fs.readFile('./build/client_secret.json', function processClientSecrets (err, content) {
   if (err) {
-    console.log('Error loading client secret file: ' + err);
-    return;
+    console.log('Error loading client secret file: ' + err)
+    return
   }
   // Authorize a client with the loaded credentials, then call the
   // Google Sheets API.
-  authorize(JSON.parse(content), buildI18N);
-});
+  authorize(JSON.parse(content), buildI18N)
+})
 
 /**
  * Create an OAuth2 client with the given credentials, and then execute the
@@ -29,22 +29,21 @@ fs.readFile('./build/client_secret.json', function processClientSecrets(err, con
  * @param {Object} credentials The authorization client credentials.
  * @param {function} callback The callback to call with the authorized client.
  */
-function authorize(credentials, callback) {
-  var clientSecret = credentials.installed.client_secret;
-  var clientId = credentials.installed.client_id;
-  var redirectUrl = credentials.installed.redirect_uris[0];
-  var auth = new googleAuth();
-  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+function authorize (credentials, callback) {
+  var clientSecret = credentials.installed.client_secret
+  var clientId = credentials.installed.client_id
+  var redirectUrl = credentials.installed.redirect_uris[0]
+  var oauth2Client = new google.auth.OAuth2(clientId, clientSecret, redirectUrl)
 
   // Check if we have previously stored a token.
-  fs.readFile(TOKEN_PATH, function(err, token) {
+  fs.readFile(TOKEN_PATH, function (err, token) {
     if (err) {
-      getNewToken(oauth2Client, callback);
+      getNewToken(oauth2Client, callback)
     } else {
-      oauth2Client.credentials = JSON.parse(token);
-      callback(oauth2Client);
+      oauth2Client.credentials = JSON.parse(token)
+      callback(oauth2Client)
     }
-  });
+  })
 }
 
 /**
@@ -55,28 +54,28 @@ function authorize(credentials, callback) {
  * @param {getEventsCallback} callback The callback to call with the authorized
  *     client.
  */
-function getNewToken(oauth2Client, callback) {
+function getNewToken (oauth2Client, callback) {
   var authUrl = oauth2Client.generateAuthUrl({
     access_type: 'offline',
     scope: SCOPES
-  });
-  console.log('Authorize this app by visiting this url: ', authUrl);
+  })
+  console.log('Authorize this app by visiting this url: ', authUrl)
   var rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
-  });
-  rl.question('Enter the code from that page here: ', function(code) {
-    rl.close();
-    oauth2Client.getToken(code, function(err, token) {
+  })
+  rl.question('Enter the code from that page here: ', function (code) {
+    rl.close()
+    oauth2Client.getToken(code, function (err, token) {
       if (err) {
-        console.log('Error while trying to retrieve access token', err);
-        return;
+        console.log('Error while trying to retrieve access token', err)
+        return
       }
-      oauth2Client.credentials = token;
-      storeToken(token);
-      callback(oauth2Client);
-    });
-  });
+      oauth2Client.credentials = token
+      storeToken(token)
+      callback(oauth2Client)
+    })
+  })
 }
 
 /**
@@ -84,110 +83,109 @@ function getNewToken(oauth2Client, callback) {
  *
  * @param {Object} token The token to store to disk.
  */
-function storeToken(token) {
+function storeToken (token) {
   try {
-    fs.mkdirSync(TOKEN_DIR);
+    fs.mkdirSync(TOKEN_DIR)
   } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw err;
+    if (err.code !== 'EEXIST') {
+      throw err
     }
   }
-  fs.writeFile(TOKEN_PATH, JSON.stringify(token));
-  console.log('Token stored to ' + TOKEN_PATH);
+  fs.writeFile(TOKEN_PATH, JSON.stringify(token))
+  console.log('Token stored to ' + TOKEN_PATH)
 }
 
 /**
  * Print the names and majors of students in a sample spreadsheet:
  * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
  */
-function buildI18N(auth) {
-  var sheets = google.sheets('v4');
+function buildI18N (auth) {
+  var sheets = google.sheets({ version: 'v4', auth })
   sheets.spreadsheets.values.get({
-    auth: auth,
     spreadsheetId: '1tfpNe1SwHQ51arbPlnE6y7rDB-JKImhGICQGsMzQtes',
-    range: 'Sheet1',
-  }, function(err, response) {
+    range: 'Sheet1'
+  }, function (err, response) {
     if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
+      console.log('The API returned an error: ' + err)
+      return
     }
-    var rows = response.values;
-    if (rows.length == 0) {
-      console.log('No data found.');
+    var rows = response.data.values
+    if (rows.length === 0) {
+      console.log('No data found.')
     } else {
-      //first value is key
-      const locales = rows[0].filter((item, itemNum) => {return itemNum>0} )
-      //console.log(locales)
+      // first value is key
+      const locales = rows[0].filter((item, itemNum) => { return itemNum > 0 })
+      // console.log(locales)
       writeLocales(locales)
-      for (let localeNum=0; localeNum< locales.length; localeNum++) {
+      for (let localeNum = 0; localeNum < locales.length; localeNum++) {
         let level = -1
         let s = '{'
         let previousToken = 'nodeOpening'
         for (let i = 1; i < rows.length; i++) {
-          let row = rows[i];
-          //console.log(row);
-          
-          if (row.length === 1) { //key name
-            //guess level
+          let row = rows[i]
+          // console.log(row)
+
+          if (row.length === 1) { // key name
+            // guess level
             const l = row[0]
               .split('')
-              .reduce((acc,index) => {
-                return acc + (index == ">")
+              .reduce((acc, index) => {
+                return acc + (index === '>')
               }, 0) - 1
-            
-              const keyName = row[0].slice(l+1)
 
-            if (l === level) {//new sibling
+            const keyName = row[0].slice(l + 1)
+
+            if (l === level) { // new sibling
               s += '},'
               s += '"' + keyName + '":{'
               previousToken = 'nodeOpening'
-            } else if (l<level) { //new parent
-              for (let j=0; j<= level-l; j++) {
+            } else if (l < level) { // new parent
+              for (let j = 0; j <= level - l; j++) {
                 s += '}'
               }
-              s += ',"' + keyName + '":{' 
+              s += ',"' + keyName + '":{'
               previousToken = 'nodeOpening'
             } else { // new child
-              s += (previousToken === 'nodeOpening') 
+              s += (previousToken === 'nodeOpening')
                 ? '"' + keyName + '":{'
                 : ',"' + keyName + '":{'
-                previousToken = 'nodeOpening'
+              previousToken = 'nodeOpening'
             }
             level = l
-          } else { //new item
-            if (previousToken!=='nodeOpening') {
+          } else { // new item
+            if (previousToken !== 'nodeOpening') {
               s += ','
             }
-            s += '"' + row[0] + '":"' + escapeQuotes(row[localeNum+1].replace("\\'","'"),'"','\\') + '"'
+            s += '"' + row[0] + '":"' + escapeQuotes(row[localeNum + 1].replace("\\'", "'"), '"', '\\') + '"'
             previousToken = 'item'
           }
         }
-        for (let i = 0; i <= level +1; i++) {
+        for (let i = 0; i <= level + 1; i++) {
           s += '}'
         }
-        //console.log(s)
+        // console.log(s)
         let jsonLocale = JSON.parse(s)
         const result = JSON.stringify(addHelp(jsonLocale, locales[localeNum]))
         writeJSON(result, locales[localeNum])
       }
     }
-  });
+  })
 }
 
-function addHelp(json,locale) {
-  const content = fs.readFileSync('./src/locales/' + locale + '/help.'+locale+'.json', 'utf8');
+function addHelp (json, locale) {
+  const content = fs.readFileSync('./src/locales/' + locale + '/help.' + locale + '.json', 'utf8')
   const helpJson = JSON.parse(content)
   json['help'] = helpJson
   return json
 }
 
-function writeJSON(s,name) {
+function writeJSON (s, name) {
   const fileName = name + '.json'
   const destPath = './src/locales/bundles/' + fileName
 
-  fs.writeFile(destPath, s, function(err) {
+  fs.writeFile(destPath, s, function (err) {
     if (err) {
-      console.log('error in locale:' +name, s)
+      console.log('error in locale:' + name, s)
       throw err
     }
     console.log('Locale ' + destPath + ' created')
@@ -195,7 +193,7 @@ function writeJSON(s,name) {
 }
 
 function writeLocales (locales) {
-  let s = `export const locales = ${JSON.stringify(locales).replace(/"/gi,"'").replace(/,/gi, ", ")}`
+  let s = `export const locales = ${JSON.stringify(locales).replace(/"/gi, "'").replace(/,/gi, ', ')}`
   s += '\n'
 
   fs.writeFile('./src/locales/locales.js', s, function (err) {

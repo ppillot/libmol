28acaa650b15fdf4fb9941c2cd1401a4084c8df0
ppillot <paulpillot@gmail.com>
2020-10-31 22:49:34 -0400

    fix: new API for RCSB search
    previous REST API is decommissionned on nov 1st 2020

--- src/components/SearchPDB2.vue
+++ src/components/SearchPDB2.vue
@@ -1,3 +1,4 @@
+/* eslint-disable template-curly-spacing */
 <template>
   <base-search-database
     :suggestions="suggestions"
@@ -42,11 +43,13 @@ export default {
         this.debouncedQuery(queryString)
       }
     },
+
     debouncedQuery: debounce(
       300,
       function (q) {
         this.querySearchAsync(q)
-      }),
+      }
+    ),
 
     querySearchAsync (queryString) {
       if (queryString.length < 2) {
@@ -59,43 +62,90 @@ export default {
         <keywords>${queryString}</keywords>
       </orgPdbQuery>`
       source = CancelToken.source()
-      axios.post('https://www.rcsb.org/pdb/rest/search',
-        xml, {
-          headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      axios.post('https://search.rcsb.org/rcsbsearch/v1/query/',
+        {
+          return_type: 'entry',
+          query: {
+            type: 'terminal',
+            service: 'text',
+            parameters: {
+              value: queryString
+            }
+          }
+        },
+        {
           cancelToken: source.token
         }
       )
         .then(function (response) {
         // console.log(response)
-          if (response.data.length > 0) {
-            let listPdbId = response.data.split('\n').slice(0, 100).join(',')
-            return axios.get('https://www.rcsb.org/pdb/rest/customReport', {
-              params: {
-                pdbids: listPdbId,
-                customReportColumns: 'structureId,structureTitle'
-              },
-              cancelToken: source.token
-            })
+
+          // RCSB Search API returns results in the form
+          // {
+          //  explain_meta_data:...
+          //  query_id: string
+          //  result_set: {
+          //    identifier: 'PDBID',
+          //    score: number
+          //    services: [number]
+          //  }
+          //  result_type: 'entry'
+          //  total_count: number
+          // }
+
+          if (response.data.result_set && response.data.result_set.length > 0) {
+            let listPdbId = response.data.result_set.map(val => val.identifier)
+
+            // Create the GraphQL query
+            // This is necessary to fetch multiple results at once.
+            // To test, use this playground:
+            // https://data.rcsb.org/graphql/index.html?query=%23%20Welcome%20to%20RCSB%20PDB%20Data%20API%3A%20GraphiQL%20interface%0A%23%0A%23%20GraphiQL%20is%20an%20in-browser%20tool%20for%20writing%2C%20validating%2C%20and%0A%23%20testing%20GraphQL%20queries.%0A%23%0A%23%20Type%20queries%20into%20this%20side%20of%20the%20screen%2C%20and%20you%20will%20see%20intelligent%0A%23%20typeaheads%20aware%20of%20the%20RCSB%20PDB%20GraphQL%20type%20schema%20and%20live%20syntax%20and%0A%23%20validation%20errors%20highlighted%20within%20the%20text.%0A%23%0A%23%20The%20%22Docs%22%20link%20on%20top%20right%20allows%20to%20browse%20the%20schema%20and%20its%20%0A%23%20documentation.%0A%23%0A%23%20GraphQL%20queries%20typically%20start%20with%20a%20%22%7B%22%20character.%20Lines%20that%20starts%0A%23%20with%20a%20%23%20are%20ignored.%0A%23%0A%23%20An%20example%20GraphQL%20query%20retrieving%20data%20at%20the%20PDB%20entry%20level%20would%20be%3A%0A%23%0A%20%20%20%20%20%7B%0A%20%20%20%20%20%20%20entries(entry_ids%3A%20%5B%224HHB%22%2C%20%221J77%22%5D)%20%7B%0A%20%20%20%20%20%20%20%20%20struct%20%7B%0A%20%20%20%20%20%20%20%20%20%20%20%20title%0A%20%20%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%20%20entry%20%7B%0A%20%20%20%20%20%20%20%20%20%20id%0A%20%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%20%20%7D%0A%20%20%20%20%20%7D%0A%23%0A%23%20Keyboard%20shortcuts%3A%0A%23%0A%23%20%20%20%20%20%20%20Run%20Query%3A%20%20Ctrl-Enter%20(or%20press%20the%20play%20button%20above)%0A%23%0A%23%20%20%20Auto%20Complete%3A%20%20Ctrl-Space%20(or%20just%20start%20typing)%0A%23%0A%23%20This%20tool%20is%20useful%20for%20prototyping%20and%20getting%20a%20feeling%20of%20how%20to%20construct%20%0A%23%20queries%20and%20to%20understand%20the%20schema.%20To%20use%20programmatically%20you%20can%20POST%20%0A%23%20your%20requests%20under%20the%20%2Fgraphql%20endpoint.%20%0A
+
+            let lGraphQLQuery = `{
+              entries(entry_ids: ["${listPdbId.join('","')}"]) {
+                struct {
+                  title
+                }
+                entry {
+                  id
+                }
+              }
+            }`
+
+            // remove blank characters from GraphQL query
+
+            lGraphQLQuery = lGraphQLQuery.replace(/\s/g, '')
+
+            // Note: axios does its own encoding when lGraphQuery is passed as
+            // an argument to its `get()` method, and it's corrupted on RCSB's
+            // side. Here we provide the URL encoded query directly in the URL.
+
+            return axios.get(
+              'https://data.rcsb.org/graphql?query=' + encodeURI(lGraphQLQuery),
+              {
+                cancelToken: source.token
+              }
+            )
           } else {
-          // self.$message.error(self.$t('messages.no_record_found'))
+            // self.$message.error(self.$t('messages.no_record_found'))
             throw new Error(this.$t('messages.no_record_found'))
           }
         }.bind(this))
         .then(function ({ data }) {
-        /* global DOMParser */
-          const parser = new DOMParser()
-          const xmlDocument = parser.parseFromString(data, 'application/xml')
-          const recordNodelist = xmlDocument.getElementsByTagName('record')
+          console.log(data)
 
-          let rep = []
-          for (var item of recordNodelist) {
-            rep.push({
-              value: item.children[1].textContent,
-              file: 'rcsb://' + item.children[0].textContent,
-              molId: item.children[0].textContent,
+          let rep = data.data.entries.map(e => {
+            const pdbid = e.entry.id
+            const title = e.struct.title
+
+            return {
+              value: title,
+              file: 'rcsb://' + pdbid,
+              molId: pdbid,
               source: 'pdb'
-            })
-          }
+            }
+          })
+
           this.suggestions = rep
 
           window.sessionStorage.setItem(`searchPDB-${queryString}`, JSON.stringify(rep))
@@ -109,9 +159,11 @@ export default {
           }
         })
     },
+
     handleSelect (index) {
       this.$store.dispatch('loadNewFile', this.suggestions[index])
     },
+
     // user has directly submitted a text. CHeck for PDB code
     handleSubmit (val) {
       if (val.length < 4 && val.length > 0) {
